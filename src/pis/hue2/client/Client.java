72326dccc1116e2eeab99058c75f7bb6bb0712c2
Cagkan Arda Sengenc <cagkan.arda.sengenc@mni.thm.de>
2021-12-27 21:56:29 +0100

    Added Closeable, Runnable Interfaces, receive & write Methods

--- src/pis/hue2/client/Client.java
+++ src/pis/hue2/client/Client.java
@@ -1,11 +1,11 @@
 package pis.hue2.client;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class Client {
-    private Socket socket;
+public class Client implements Closeable {
+    private final Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
     private String clientID;
@@ -13,6 +13,13 @@ public class Client {
     public Client(Socket socket, String clientID) {
         this.socket = socket;
         this.clientID = clientID;
+        try {
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
@@ -22,5 +29,57 @@ public class Client {
             System.out.println("SA");
         }
 
+    }
+
+
+    public void writeMessage() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (socket.isConnected()) {
+            String message = scanner.nextLine();
+            try {
+                bufferedWriter.write(message);
+                bufferedWriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Error occurred while sending a message!!");
+            } finally {
+                close();
+            }
+        }
+    }
+
+
+    public void receiveMessage() throws IOException {
+        String receivedMessage;
+        while (socket.isConnected()) {
+            try {
+                receivedMessage = bufferedReader.readLine();
+                System.out.println(receivedMessage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                close();
+            }
+
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (bufferedReader != null && bufferedWriter != null && socket != null) {
+            bufferedReader.close();
+            bufferedWriter.close();
+            socket.close();
+        } else {
+            System.err.println("Could not close one of the streams!");
+        }
+    }
+
+    public String getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(String clientID) {
+        this.clientID = clientID;
     }
 }
--- src/pis/hue2/client/ClientPool.java
+++ src/pis/hue2/client/ClientPool.java
@@ -1,8 +1,85 @@
 package pis.hue2.client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * @author ardasengenc
  */
-public class ClientPool {
+public class ClientPool implements Runnable, Closeable {
+    private static ArrayList<Client> clients = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
 
+    public ClientPool(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    public void removeClient(Client client) {
+        if (!clients.isEmpty()) {
+            clients.remove(client);
+            System.out.println(client.getClientID() + " has left the chat!");
+        } else {
+            System.out.println("Nobody is in the room!");
+        }
+    }
+
+    public void uploadFiles() {
+
+    }
+
+    public void deleteFiles() {
+
+    }
+
+    public void showFiles() {
+
+    }
+
+
+    /**
+     * Closes this stream and releases any system resources associated
+     * with it. If the stream is already closed then invoking this
+     * method has no effect.
+     *
+     * <p> As noted in {@link AutoCloseable#close()}, cases where the
+     * close may fail require careful attention. It is strongly advised
+     * to relinquish the underlying resources and to internally
+     * <em>mark</em> the {@code Closeable} as closed, prior to throwing
+     * the {@code IOException}.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    public void close() throws IOException {
+        if (bufferedReader != null && bufferedWriter != null && socket != null) {
+            bufferedReader.close();
+            bufferedWriter.close();
+            socket.close();
+        } else {
+            System.err.println("Could not close one of the streams!");
+        }
+    }
+
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
 }
