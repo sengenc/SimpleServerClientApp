e4f9ff731c6825fc49c3e1e7c3c68093688b7d5a
Cagkan Arda Sengenc <cagkan.arda.sengenc@mni.thm.de>
2021-12-28 17:19:47 +0100

    Server Class

---
+++ src/pis/hue2/server/Server.java
@@ -0,0 +1,44 @@
+package pis.hue2.server;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @author ardasengenc
+ */
+public class Server implements Runnable, Closeable {
+
+    /**
+     * Closes this stream and releases any system resources associated
+     * with it. If the stream is already closed then invoking this
+     * method has no effect.
+     *
+     * <p> As noted in {@link AutoCloseable#close()}, cases where the
+     * close may fail require careful attention. It is strongly advised
+     * to relinquish the underlying resources and to internally
+     * <em>mark</em> the {@code Closeable} as closed, prior to throwing
+     * the {@code IOException}.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    public void close() throws IOException {
+
+    }
+
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+}
